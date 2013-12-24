cf10301a747366a1b0aaa8907a93a99b3ef954fb
Christopher Corley <cscorley@ua.edu>
2013-12-24 16:34:09 -0600

    Move some setup code around
    Conflicts:
    	Makefile

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ help:
 	@echo "install - install + compile native things."
 	@echo "update  - Download update for all plugins."
 
-install: symlink submodules
+install: submodules
 
 submodules:
 	git submodule init
@@ -15,33 +15,3 @@ submodules:
 
 update:
 	git submodule foreach git pull origin master 
-
-symlinkforce:
-	for file in `ls -A $(CURDIR)/dots`; do \
-		if [[ -d ~/$$file ]] ; then \
-			rm -rf ~/$$file ; \
-		fi ; \
-		ln -sf "$(CURDIR)/dots/$$file" ~/$$file ; \
-	done
-
-symlink:
-	for file in `ls -A $(CURDIR)/dots`; do \
-		if [[ -d ~/$$file ]] ; then \
-			if [[ -L ~/$$file ]] ; then \
-				rm -i ~/$$file ; \
-				if [[ ! -e ~/$$file ]] ; then \
-					ln -si "$(CURDIR)/dots/$$file" ~/$$file ; \
-				fi ; \
-			else \
-				echo "Directory ~/$$file exists, please remove it."; \
-				exit; \
-			fi ; \
-		else \
-			ln -si "$(CURDIR)/dots/$$file" ~/$$file ; \
-		fi ; \
-	done
-
-test:
-	for file in `ls -A $(CURDIR)/dots`; do \
-		echo "ln -si" "$(CURDIR)/dots/$$file" "~/$$file" ; \
-	done
--- setup.sh
+++ setup.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+set -e
+
+# I haven't tested any of these functions. No fucks given.
+function symlink_interactive(){
+    for file in `ls -A ${PWD}/dots`; do
+        if [[ -d ~/${file} ]] ; then
+            if [[ -L ~/${file} ]] ; then
+                rm -i ~/${file}
+            else
+                mv -i ~/${file} ~/${file}.backup
+            fi
+
+            if [[ ! -e ~/${file} ]] ; then
+                ln -si "${PWD}/dots/${file}" ~/${file}
+            fi
+        else
+            ln -si "${PWD}/dots/${file}" ~/${file}
+        fi
+    done
+}
+
+function symlink_force(){
+    for file in `ls -A ${PWD}/dots`; do
+        if [[ -d ~/${file} ]] ; then
+            if [[ ! -L ~/${file} ]]; then
+                echo "Moving ~/${file} to ~/${file}.backup"
+                mv -f ~/${file} ~/${file}.backup
+            fi
+        fi
+        ln -sf "${PWD}/dots/${file}" ~/${file}
+    done
+}
 
 if [ -d ~/.dotfiles ]
 then
@@ -17,9 +49,15 @@ fi
 echo "Cloning repo..."
 /usr/bin/env git clone https://github.com/cscorley/dotfiles.git ~/.dotfiles
 
-
 echo "Installing..."
 cd ~/.dotfiles
+
+if [ "${1}" == "-f" ]; then
+    symlink_force()
+else
+    symlink_interactive()
+fi
+
 make install
 
 echo "Done."
