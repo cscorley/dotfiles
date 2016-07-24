33d34523b0b84508939535b8855bb82bdcb08ca6
Christopher Corley <cscorley@ua.edu>
2016-07-24 13:12:17 -0400

    some improvements to setup

--- setup.sh
+++ setup.sh
@@ -3,66 +3,66 @@
 
 function symlink_interactive(){
     for file in `ls -A ${PWD}/dots`; do
-        if [[ -d ~/${file} ]] ; then
-            if [[ -L ~/${file} ]] ; then
-                rm -i ~/${file}
+        if [[ -d ${HOME}/${file} ]] ; then
+            if [[ -L ${HOME}/${file} ]] ; then
+                rm -i ${HOME}/${file}
             else
-                mv -i ~/${file} ~/${file}.backup
+                mv -i ${HOME}/${file} ${HOME}/${file}.backup
             fi
 
-            if [[ ! -e ~/${file} ]] ; then
-                ln -si "${PWD}/dots/${file}" ~/${file}
+            if [[ ! -e ${HOME}/${file} ]] ; then
+                ln -si "${PWD}/dots/${file}" ${HOME}/${file}
             fi
         else
-            ln -si "${PWD}/dots/${file}" ~/${file}
+            ln -si "${PWD}/dots/${file}" ${HOME}/${file}
         fi
     done
 }
 
-function symlink_force(){
-    for file in `ls -A ${PWD}/dots`; do
-        if [[ -d ~/${file} ]] ; then
-            if [[ ! -L ~/${file} ]]; then
-                echo "-> Moving ~/${file} to ~/${file}.backup"
-                mv -f ~/${file} ~/${file}.backup
-            fi
-        fi
-        ln -sf "${PWD}/dots/${file}" ~/${file}
-    done
-}
+echo "-----> SETUP! --------------------------------------------------"
+echo "----->        This will interactively setup files from dots into ${HOME}."
+echo "----->        To skip interactivity, pipe 'yes' into this setup."
+echo "-----> SETUP! --------------------------------------------------"
+echo ""
 
-if [ -d ~/.dotfiles ]; then
-    echo "!> A ~/.dotfiles directory already exists. Not cloning repo."
+os=`uname -o`
+echo "!> Found OS: ${os}"
+
+if [ -d ${HOME}/.dotfiles ]; then
+    echo "!> A '${HOME}/.dotfiles' directory already exists. Not cloning repo."
     echo "!> Please remove and re-run if necessary."
 else
     echo "-> Cloning main dotfile repo..."
-    git clone https://github.com/cscorley/dotfiles.git ~/.dotfiles
+    git clone https://github.com/cscorley/dotfiles.git ${HOME}/.dotfiles
 fi
 
 echo "-> Installing..."
-cd ~/.dotfiles
+cd ${HOME}/.dotfiles
 
-if [ "${1}" == "-f" ]; then
-    echo "-> Forcing symlink creation. Godspeed!"
-    symlink_force
+if [ "${os}" == "Cygwin" ]; then
+    echo "-> Interactively copying dotfiles instead of symlinking..."
+    cp -ivR dots/.* ${HOME}
 else
-    echo "-> Interactively symlinking ~/.dotfiles..."
+    echo "-> Interactively symlinking dotfiles..."
     symlink_interactive
 fi
 
-mkdir -p ~/.vim/tmp/{backup,swap,undo}
+mkdir -p ${HOME}/.vim/tmp/{backup,swap,undo}
 
 echo "-> Running update script to clone git repos and install Vundle plugins"
 
 ./update.sh
 
 echo "-> Copying files from 'manual' directory"
-cp manual/mortaldouchebag.zsh-theme ~/.oh-my-zsh/themes/
+cp manual/mortaldouchebag.zsh-theme ${HOME}/.oh-my-zsh/themes/
 
-echo "-> Installing powerline font"
-mkdir -p ~/.fonts/
-cd ~/.fonts
-curl -O https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
-fc-cache -vf ~/.fonts/
+if [ "${os}" == "Linux" ]; then
+    echo "-> Installing powerline font"
+    mkdir -p ${HOME}/.fonts/
+    cd ${HOME}/.fonts
+    curl -O https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+
+    fc-cache -vf ${HOME}/.fonts/
+fi
 
 echo "-> Done."
